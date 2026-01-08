3263c6c7e7f2770a244b331012702576bc82aa9c
Nkasozi <nkasozi@gmail.com>
2026-01-08 22:48:20 +0300

    Remove .svelte-kit from git history and add to .gitignore

--- frontend/.svelte-kit/output/client/_app/immutable/nodes/5.D4G_-AZk.js
+++
@@ -1,1 +0,0 @@
-import"../chunks/CWj6FrbW.js";import"../chunks/69_IOA4Y.js";const n=Object.freeze(Object.defineProperty({__proto__:null},Symbol.toStringTag,{value:"Module"}));function r(e){}export{r as component,n as universal};
--- frontend/.svelte-kit/output/server/nodes/3.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 3;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/crud-test/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/3.DG8pIUC0.js","_app/immutable/chunks/CWj6FrbW.js","_app/immutable/chunks/69_IOA4Y.js","_app/immutable/chunks/DIeogL5L.js","_app/immutable/chunks/DhAImXhw.js","_app/immutable/chunks/DjIM3LOd.js","_app/immutable/chunks/DIZdboBT.js","_app/immutable/chunks/CeJ_7Afs.js","_app/immutable/chunks/DCA2aStm.js","_app/immutable/chunks/BJia4XKL.js","_app/immutable/chunks/CiPvQyKl.js","_app/immutable/chunks/BcHKa6QZ.js","_app/immutable/chunks/CvESoDjb.js"];
-export const stylesheets = ["_app/immutable/assets/EntityCrudWrapper.wgjcJtws.css","_app/immutable/assets/3.241F-1yh.css"];
-export const fonts = [];
